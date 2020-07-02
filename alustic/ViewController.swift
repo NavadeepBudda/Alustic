67ee755d1b39fb2ab3b5da3790a33fcd0f20e788
Navadeep Budda <navadeep.budda@gmail.com>
2020-07-01 21:27:38 -0400

    Initial Commit

---
+++ alustic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alustic
+//
+//  Created by Naveen Budda on 7/1/20.
+//  Copyright © 2020 Navadeep Budda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
